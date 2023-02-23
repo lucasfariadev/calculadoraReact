77aa68f87df4b3d040f97535727f5632fbf71932
lucasfariadev <lucas_faria@outlook.com>
2023-02-23 06:57:11 -0300

    adicionado desafio de criação de calculadora com React

---
+++ calculadora/src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAF0;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-wight: 700px;
+    flex: 1;
+
+    &:hover{
+        opacity: 0.6;
+    }
+
+
+
+
+`
